73fba1e5d8ec7cd158f144c48180b7273dbe4735
henrfarias <henrique.biasibetti@gmail.com>
2021-09-17 14:17:53 -0300

    chore: exclude some folders without classes or methods

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,31 @@
-import { Config } from '@jest/types'
+import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-	preset: 'ts-jest',
-	verbose: true,
-	clearMocks: true,
-	coverageDirectory: 'coverage',
-	collectCoverageFrom: ['src/**/*.ts', '!src/framework/**/*.ts'],
-	coveragePathIgnorePatterns: ['/node_modules/'],
-	testEnvironment: 'node',
-	coverageProvider: 'v8',
-	rootDir: './',
-	testMatch: ['**/tests/**/*.(spec|test).[jt]s'],
-	setupFiles: ['./jest-setup-file.ts'],
-	moduleNameMapper: {
-		'@domain/(.*)': '<rootDir>/src/domain/$1',
-		'@business/(.*)': '<rootDir>/src/business/$1',
-		'@controller/(.*)': '<rootDir>/src/controller/$1',
-		'@framework/(.*)': '<rootDir>/src/framework/$1',
-		'@shared/(.*)': '<rootDir>/src/shared/$1',
-		'@tests/(.*)': '<rootDir>/tests/$1',
-		'@root/(.*)': '<rootDir>/$1',
-	},
-}
+  preset: 'ts-jest',
+  verbose: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/framework/**/*.ts',
+    '!src/business/dto/**/*.ts',
+    '!src/business/useCases/*.ts',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  rootDir: './',
+  testMatch: ['**/tests/**/*.(spec|test).[jt]s'],
+  setupFiles: ['./jest-setup-file.ts'],
+  moduleNameMapper: {
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@business/(.*)': '<rootDir>/src/business/$1',
+    '@controller/(.*)': '<rootDir>/src/controller/$1',
+    '@framework/(.*)': '<rootDir>/src/framework/$1',
+    '@shared/(.*)': '<rootDir>/src/shared/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1',
+    '@root/(.*)': '<rootDir>/$1',
+  },
+};
 
-export default config
+export default config;
